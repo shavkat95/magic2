41ab06c717efa4c500265bc3168fed57b632a977
unknown <shavkat95@icloud.com>
2023-09-21 23:59:30 +0200

    serving images

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,20 @@
 import pkg from 'pg';
 const { Pool, Client } = pkg;
 
+import express from 'express'
+const app = express();
+const PORT = 5000;
+
+// http://localhost:5000/img1.avif
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+
 // const pool = new Pool({
 //   user: "postgres",
 //   host: "0.0.0.0",
